8472702d79c89ebd90d23ac53fd2e40cec75711f
Bugee <>
2025-11-08 22:09:54 +0800

    feat(mcp): implement streamable HTTP client and unit tests

---
+++ src/utils/mcp/index.ts
@@ -0,0 +1,174 @@
+import type { ClientOptions } from '@modelcontextprotocol/sdk/client/index';
+import { Client } from '@modelcontextprotocol/sdk/client/index';
+import { StreamableHTTPClientTransport } from '@modelcontextprotocol/sdk/client/streamableHttp';
+import type { RequestOptions } from '@modelcontextprotocol/sdk/shared/protocol';
+import type {
+  CallToolRequest,
+  CallToolResult,
+  ClientResult,
+  CreateMessageRequest,
+  ListToolsRequest,
+  ListToolsResult,
+  ProgressNotification,
+} from '@modelcontextprotocol/sdk/types';
+import {
+  CallToolResultSchema,
+  CreateMessageRequestSchema,
+  ListToolsResultSchema,
+  ProgressNotificationSchema,
+} from '@modelcontextprotocol/sdk/types';
+import type { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
+
+export const mcpBaseUrl = import.meta.env.VITE_MCP_BASE_URL || '';
+const mcpEndpoint = import.meta.env.VITE_MCP_ENDPOINT || '';
+
+export interface CreateMcpClientOptions {
+  baseUrl?: string;
+  mcpEndpoint?: string;
+  version?: string;
+  requestTimeout?: number;
+  useWebsocket?: boolean;
+  progressHandler?: (progressNotification: ProgressNotification) => void;
+  samplingHandler?: (createMessageRequest: CreateMessageRequest) => Promise<ClientResult>;
+  authProvider?: () => Promise<string>;
+}
+
+export class MyMcpClient {
+  private readonly options: CreateMcpClientOptions;
+
+  constructor(options: CreateMcpClientOptions) {
+    this.options = options;
+  }
+
+  public getClient = async (): Promise<Client> => {
+    const url = new URL(this.options.baseUrl);
+    url.pathname = this.options.mcpEndpoint;
+    let accessToken = '';
+    if (this.options.authProvider) {
+      accessToken = await this.options.authProvider();
+    }
+    const transport = new StreamableHTTPClientTransport(url, {
+      requestInit: {
+        headers: {
+          Authorization: accessToken,
+        },
+      },
+    });
+
+    const clientOptions: ClientOptions = {
+      capabilities: {},
+    };
+
+    if (this.options.samplingHandler) {
+      clientOptions.capabilities.sampling = {};
+    }
+
+    const client = new Client(
+      {
+        name: 'typescript client',
+        version: '1.20.2',
+      },
+      clientOptions,
+    );
+
+    if (this.options.samplingHandler) {
+      client.setRequestHandler(CreateMessageRequestSchema, this.options.samplingHandler);
+    }
+
+    if (this.options.progressHandler) {
+      client.setNotificationHandler(ProgressNotificationSchema, this.options.progressHandler);
+    }
+
+    await client.connect(transport);
+    return client;
+  };
+
+  public close = async (client: Client): Promise<void> => {
+    if (client.transport instanceof StreamableHTTPClientTransport) {
+      await client.transport.terminateSession();
+    }
+    await client.close();
+  };
+
+  public listTools = async (): Promise<ListToolsResult> => {
+    const client = await this.getClient();
+    const toolsRequest: ListToolsRequest = {
+      method: 'tools/list',
+      params: {},
+    };
+    try {
+      return await client.request(toolsRequest, ListToolsResultSchema);
+    } finally {
+      await this.close(client);
+    }
+  };
+
+  public doResult = (toolName: string, toolResult: CallToolResult): any => {
+    let callResult: any;
+    if (!toolResult) {
+      throw new Error(`call ${toolName} error: Result is null.`);
+    } else if (toolResult.structuredContent) {
+      callResult = toolResult.structuredContent;
+    } else {
+      if (toolResult.content && Array.isArray(toolResult.content)) {
+        const content = toolResult.content;
+        if (content.length === 0 || content[0].type !== 'text') {
+          console.error(`callTool error:${toolName}`, toolResult);
+          throw new Error(`call ${toolName} error: Unable to process the result.`);
+        } else {
+          callResult = JSON.parse(content[0].text);
+        }
+      }
+    }
+
+    if (toolResult.isError) {
+      if (callResult.error) {
+        throw new Error(`call ${toolName} error: ${callResult.error.message}`);
+      } else {
+        const message = JSON.stringify(callResult);
+        throw new Error(`call ${toolName} error: ${message}`);
+      }
+    }
+  };
+
+  public callTool = async (toolName: string, data: any, options?: RequestOptions, client?: Client): Promise<any> => {
+    const params: Record<string, any> = data;
+    const toolsRequest: CallToolRequest['params'] = {
+      name: toolName,
+      arguments: params,
+    };
+    if (client) {
+      const toolResult = await client.callTool(toolsRequest, CallToolResultSchema, options);
+      return this.doResult(toolName, toolResult);
+    } else {
+      const client = await this.getClient();
+      try {
+        const toolResult = await client.callTool(toolsRequest, CallToolResultSchema, options);
+        return this.doResult(toolName, toolResult);
+      } finally {
+        await this.close(client);
+      }
+    }
+  };
+}
+
+export const postRequest = async <T = any>(url: string, data: any = {}, config?: AxiosRequestConfig): Promise<T> => {
+  try {
+    const response = await axios.post<T>(url, data, config);
+    return response.data;
+  } catch (error: any) {
+    console.error('POST error:', error);
+    throw error;
+  }
+};
+
+export const getMcpClient = async (options?: CreateMcpClientOptions) => {
+  if (options && !options.baseUrl) {
+    options.baseUrl = mcpBaseUrl;
+  }
+  if (options && !options.mcpEndpoint) {
+    options.mcpEndpoint = mcpEndpoint;
+  }
+  return new MyMcpClient(options);
+};
