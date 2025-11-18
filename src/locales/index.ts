2fd2b2f9eaea0297ce62b6cdd6ff2f89c9d2e8cd
Bugee <>
2025-11-18 11:25:19 +0800

    chore: change default language to en_US

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -42,7 +42,7 @@ const importMessages = computed(() => {
 
 export const i18n = createI18n({
   legacy: false,
-  locale: useLocalStorage(localeConfigKey, 'zh_CN').value || languages.value[0] || 'zh_CN',
+  locale: useLocalStorage(localeConfigKey, 'en_US').value || languages.value[0] || 'zh_CN',
   fallbackLocale: 'zh_CN',
   messages: importMessages.value,
   globalInjection: true,
