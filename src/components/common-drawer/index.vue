f1ef7e5e5660fddb75648c7491f69608598c66de
Bugee <>
2025-12-30 19:02:04 +0800

    feat: add common-drawer and form-drawer components

---
+++ src/components/common-drawer/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <t-drawer
+    v-model:visible="drawerVisible"
+    :header="title"
+    :close-btn="showCloseBtn"
+    :size="size"
+    :confirm-btn="null"
+    :cancel-btn="null"
+    class="common-drawer"
+    @close-btn-click="handleClose"
+  >
+    <slot />
+    <template #footer>
+      <slot name="footer">
+        <t-space>
+          <loading-button v-if="showConfirmBtn" theme="primary" @click="handleConfirm">
+            {{ confirmText }}
+          </loading-button>
+          <t-button v-if="showCancelBtn" theme="default" variant="base" @click="handleCancel">
+            {{ cancelText }}
+          </t-button>
+        </t-space>
+      </slot>
+    </template>
+  </t-drawer>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  visible: boolean;
+  title?: string;
+  size?: string;
+
+  confirmText?: string;
+  cancelText?: string;
+  showConfirmBtn?: boolean;
+  showCancelBtn?: boolean;
+  showCloseBtn?: boolean;
+}
+
+interface Emits {
+  (e: 'update:visible', value: boolean): void;
+  (e: 'confirm', data?: any): void;
+  (e: 'cancel', data?: any): void;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: 'drawer',
+  size: 'medium',
+  confirmText: 'confirm',
+  cancelText: 'cancel',
+  showConfirmBtn: true,
+  showCancelBtn: true,
+  showCloseBtn: true,
+});
+
+const emit = defineEmits<Emits>();
+
+const drawerVisible = computed({
+  get: () => props.visible,
+  set: (value) => emit('update:visible', value),
+});
+
+const handleConfirm = async () => {
+  await new Promise((resolve, reject) => {
+    emit('confirm', { resolve, reject });
+  });
+};
+
+const handleCancel = () => {
+  drawerVisible.value = false;
+  emit('cancel');
+};
+
+const handleClose = () => {
+  drawerVisible.value = false;
+  emit('cancel');
+};
+</script>
+<style lang="less" scoped></style>
---
+++ src/components/form-drawer/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <common-drawer
+    v-model:visible="drawerVisible"
+    :title="title"
+    :size="size"
+    :confirm-text="confirmText"
+    :show-confirm-btn="showConfirmBtn"
+    :show-cancel-btn="showCancelBtn"
+    :show-close-btn="showCloseBtn"
+    @confirm="handleConfirm"
+    @cancel="handleCancel"
+  >
+    <t-form ref="formRef" :data="formData" :rules="formRules">
+      <slot />
+    </t-form>
+  </common-drawer>
+</template>
+<script setup lang="ts">
+import type { FormInstanceFunctions, FormRule } from 'tdesign-vue-next';
+import { computed, ref } from 'vue';
+
+import CommonDrawer from '@/components/common-drawer/index.vue';
+
+interface Props {
+  visible: boolean;
+  title?: string;
+  size?: string;
+
+  confirmText?: string;
+  cancelText?: string;
+  showConfirmBtn?: boolean;
+  showCancelBtn?: boolean;
+  showCloseBtn?: boolean;
+  formData: any;
+  formRules: Record<string, FormRule[]>;
+}
+
+interface Emits {
+  (e: 'update:visible', value: boolean): void;
+  (e: 'validate-success', data: any): void;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  showConfirmBtn: true,
+  showCancelBtn: true,
+  showCloseBtn: true,
+});
+
+const emit = defineEmits<Emits>();
+
+const drawerVisible = computed({
+  get: () => props.visible,
+  set: (value) => emit('update:visible', value),
+});
+
+const formRef = ref<FormInstanceFunctions>();
+
+const handleConfirm = async (params: any) => {
+  const { resolve, reject } = params;
+  const valid = await formRef.value?.validate();
+  if (valid === true) {
+    emit('validate-success', { resolve, reject });
+  } else {
+    resolve();
+  }
+};
+
+const handleCancel = () => {
+  formRef.value.reset();
+};
+</script>
